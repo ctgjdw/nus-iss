06dab66db11e4c441ecc52a16adef161f9531e06
gohchunteck@hotmail.com <gohchunteck@hotmail.com>
2023-10-03 22:53:32 +0800

    ws1

---
+++ workshop01/web.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+from service import get_sentence
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", sentence=get_sentence())
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
