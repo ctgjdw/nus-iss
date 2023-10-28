c5e1b42d5e3d95de8a1b310e6281ee926e044f99
gohchunteck@hotmail.com <gohchunteck@hotmail.com>
2023-10-28 13:45:17 +0800

    update devfile for oc deploy

--- workshop01/task1/main.js
+++ workshop01/task1/main.js
@@ -4,39 +4,37 @@ const { create } = require('express-handlebars')
 const express = require('express')
 
 const rnd = (range = 14, total = 4) => {
-	const nums = []
+    const nums = []
 
-	for (let i = 0; i < range; i++)
-		nums.push(i)
+    for (let i = 0; i < range; i++) nums.push(i)
 
-	for (let i = 0; i < range; i++) {
-		const idx = Math.floor(Math.random() * range)
-		const t = nums[i]
-		nums[i] = nums[idx]
-		nums[idx] = t
-	}
+    for (let i = 0; i < range; i++) {
+        const idx = Math.floor(Math.random() * range)
+        const t = nums[i]
+        nums[i] = nums[idx]
+        nums[idx] = t
+    }
 
-	return nums.splice(0, total)
+    return nums.splice(0, total)
 }
 
 let opt = {}
 
 try {
-	opt = cliOptions([ 
-		{ name: 'port', alias: 'p', type: Number },
-		{ name: 'name', alias: 'n', type: String },
-		{ name: 'hash', type: String },
-	])
+    opt = cliOptions([
+        { name: 'port', alias: 'p', type: Number },
+        { name: 'name', alias: 'n', type: String },
+        { name: 'hash', type: String },
+    ])
 } catch (err) {
-	console.error('Parse CLI options error: ', err)
-	if (!process.env.HEROKU_WORKAROUND)
-		throw(err)
-	opt = {}
+    console.error('Parse CLI options error: ', err)
+    if (!process.env.HEROKU_WORKAROUND) throw err
+    opt = {}
 }
 
 const port = opt['port'] || opt['p'] || parseInt(process.env.PORT) || 3000
 const instanceName = opt['name'] || opt['n'] || process.env.INSTANCE_NAME || ''
-const instanceHash = opt['hash'] || process.env.INSTANCE_HASH || '' 
+const instanceHash = opt['hash'] || process.env.INSTANCE_HASH || ''
 
 const app = express()
 
@@ -46,19 +44,18 @@ app.set('view engine', 'hbs')
 app.use(morgan('common'))
 
 app.get('/healthz', (req, resp) => {
-	resp.status(204).end()
+    resp.status(204).end()
 })
 
 app.use(express.static(__dirname + '/public'))
 
-app.get([ '/', '/index.html' ], (req, resp) => {
-	const total = parseInt(req.query['num']) || 4
-	const dovs = rnd(14, total)
-	resp.status(200).type('text/html')
-	resp.render('index', { dovs, instanceName, instanceHash })
+app.get(['/', '/index.html'], (req, resp) => {
+    const total = parseInt(req.query['num']) || 4
+    const dovs = rnd(14, total)
+    resp.status(200).type('text/html')
+    resp.render('index', { dovs, instanceName, instanceHash })
 })
 
 app.listen(port, () => {
-	console.info(`Application started on port ${port} at ${new Date()}`)
+    console.info(`Application started on port ${port} at ${new Date()}!`)
 })
-
